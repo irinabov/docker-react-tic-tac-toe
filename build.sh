b31a4adf5fabffa16adf9883ec0157db53bb1cb3
Irina Boverman <iboverma@redhat.com>
2018-01-10 16:11:42 -0500

    Updated installed packages

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Another test
 home=$PWD
 dnf -y update
-dnf -y install nodejs git sed
+dnf -y install nodejs git sed python2 gcc-c++
 git clone https://github.com/irinabov/react-tic-tac-toe.git
 npm install forever -g
 cd react-tic-tac-toe
